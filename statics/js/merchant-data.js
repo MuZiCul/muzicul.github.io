e3b36d13215941695271e703faf7eeb8737ab23e
obama <obama110@163.com>
2025-03-31 15:48:58 +0800

    温泉更新

--- statics/js/merchant-data.js
+++ statics/js/merchant-data.js
@@ -5,38 +5,38 @@ const merchantData = {
               {
             type: "樱花",
             players: [
-                { id: "晚江词", uid: "448710538", sell:"1.9倍", at:"", note: "", leaveTime: "4.1号20.36" },
+                { id: "晚江词", uid: "448710538", sell:"1.9", at:"", note: "", leaveTime: "4.1号20.36" },
             ]
         },{
             type: "连翘",
             players: [
-                { id: "招财小芙", uid: "1362694097", sell:"", at:"", note: "", leaveTime: "4.1号11.48" },
-                { id: "薄雾插画烧仙草", uid: "1806991113", sell:"1.9倍", at:"", note: "", leaveTime: "4.2号9.00" },
+                { id: "招财小芙", uid: "1362694097", sell:"2.0", at:"", note: "", leaveTime: "4.1号11.48" },
+                { id: "薄雾插画烧仙草", uid: "1806991113", sell:"1.9", at:"", note: "", leaveTime: "4.2号9.00" },
             ]
         },{
             type: "粉目菊",
             players: [
-                { id: "小雪LLL", uid: "1429665992", sell:"", at:"", note: "", leaveTime: "3.31号23.00" },
+                { id: "小雪LLL", uid: "1429665992", sell:"2.0", at:"", note: "", leaveTime: "3.31号23.00" },
             ]
         },{
             type: "莲花掌",
             players: [
-                { id: "久玥丨", uid: "749000844", sell:"", at:"", note: "", leaveTime: "4.1号11.00" },
+                { id: "久玥丨", uid: "749000844", sell:"2.0", at:"", note: "", leaveTime: "4.1号11.00" },
             ]
         },{
             type: "仙客来",
             players: [
-                { id: "窈窈", uid: "1161014933", sell:"1.9倍", at:"", note: "", leaveTime: "4.1号10.37" },
+                { id: "窈窈", uid: "1161014933", sell:"1.9", at:"", note: "", leaveTime: "4.1号10.37" },
             ]
         },{
             type: "黄百合",
             players: [
-                { id: "十而猴", uid: "1161014933", sell:"", at:"@杏丝", note: "", leaveTime: "4.1号22.37" },
+                { id: "十而猴", uid: "1161014933", sell:"2.0", at:"@杏丝", note: "", leaveTime: "4.1号22.37" },
             ]
         },{
             type: "丁香花",
             players: [
-                { id: "DT丨Silver", uid: "720457468", sell:"", at:"@温泉看公告", note: "送猫头鹰", leaveTime: "4.1号22.37" },
+                { id: "DT丨Silver", uid: "720457468", sell:"2.0", at:"@温泉看公告", note: "送猫头鹰", leaveTime: "4.1号22.37" },
             ]
         },
     ]

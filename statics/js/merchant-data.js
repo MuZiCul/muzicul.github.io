eebd8370fc1fb2fb84eab2128d900192dd6269ec
obama <obama110@163.com>
2025-03-31 15:16:37 +0800

    温泉更新

--- statics/js/merchant-data.js
+++ statics/js/merchant-data.js
@@ -6,12 +6,28 @@ const merchantData = {
             type: "樱花",
             players: [
                 { id: "旧版刊", uid: "295105454", note: "送猫头鹰", leaveTime: "3.31号20.48" },
+                { id: "晚江词", uid: "448710538", note: "", leaveTime: "4.1号20.36" },
             ]
         },{
             type: "连翘",
             players: [
                 { id: "招财小芙", uid: "1362694097", note: "", leaveTime: "4.1号11.48" },
-                { id: "zhangwudi", uid: "535262032", note: "", leaveTime: "3.31号8.56" },
+                { id: "薄雾插画烧仙草", uid: "1806991113", note: "", leaveTime: "4.2号9.00" },
+            ]
+        },{
+            type: "粉目菊",
+            players: [
+                { id: "小雪LLL", uid: "1429665992", note: "", leaveTime: "3.31号23.00" },
+            ]
+        },{
+            type: "莲花掌",
+            players: [
+                { id: "久玥丨", uid: "749000844", note: "", leaveTime: "4.1号11.00" },
+            ]
+        },{
+            type: "仙客来",
+            players: [
+                { id: "窈窈", uid: "1161014933", note: "", leaveTime: "4.1号10.37" },
             ]
         },
     ]

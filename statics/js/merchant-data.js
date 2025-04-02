3ee7337a4a9cf73e95c5b1b5953960f81508e88a
obama <obama110@163.com>
2025-04-02 13:13:22 +0800

    增加贡献榜

--- statics/js/merchant-data.js
+++ statics/js/merchant-data.js
@@ -5,12 +5,11 @@ const merchantData = {
               {
             type: "樱花",
             players: [
-                { id: "晚江词", uid: "448710538", sell:"1.9", at:"", note: "", leaveTime: "4.1号20.36" },
+                { id: "000", uid: "000", sell:"1.9", at:"", note: "", leaveTime: "4.1号20.36" },
             ]
         },{
             type: "连翘",
             players: [
-                { id: "招财小芙", uid: "1362694097", sell:"2.0", at:"", note: "", leaveTime: "4.1号11.48" },
                 { id: "炸诈吒弹", uid: "724040624", sell:"2.0", at:"", note: "", leaveTime: "4.2号21.03" },
                 { id: "女乃_米唐", uid: "322908505", sell:"2.0", at:"", note: "", leaveTime: "4.3号10.34" },
                 { id: "薄雾插画烧仙草", uid: "1806991113", sell:"1.9", at:"", note: "", leaveTime: "4.2号9.00" },
@@ -18,22 +17,22 @@ const merchantData = {
         },{
             type: "粉目菊",
             players: [
-                { id: "小雪LLL", uid: "1429665992", sell:"2.0", at:"", note: "", leaveTime: "3.31号23.00" },
+                { id: "000", uid: "000", sell:"2.0", at:"", note: "", leaveTime: "3.31号23.00" },
             ]
         },{
             type: "莲花掌",
             players: [
-                { id: "久玥丨", uid: "749000844", sell:"2.0", at:"", note: "", leaveTime: "4.1号11.00" },
+                { id: "000", uid: "000", sell:"2.0", at:"", note: "", leaveTime: "4.1号11.00" },
             ]
         },{
             type: "仙客来",
             players: [
-                { id: "窈窈", uid: "1161014933", sell:"1.9", at:"", note: "", leaveTime: "4.1号10.37" },
+                { id: "000", uid: "000", sell:"1.9", at:"", note: "", leaveTime: "4.1号10.37" },
             ]
         },{
             type: "黄百合",
             players: [
-                { id: "十而猴", uid: "1161014933", sell:"2.0", at:"@杏丝", note: "", leaveTime: "4.1号22.37" },
+                { id: "000", uid: "000", sell:"2.0", at:"", note: "", leaveTime: "4.1号22.37" },
             ]
         },{
             type: "丁香花",
@@ -44,7 +43,7 @@ const merchantData = {
         },{
             type: "康乃馨",
             players: [
-                { id: "猫饼oxo", uid: "942674015", sell:"2.0", at:"", note: "", leaveTime: "4.1号18.00" },
+                { id: "000", uid: "000", sell:"2.0", at:"", note: "", leaveTime: "4.1号18.00" },
             ]
         },{
             type: "雪滴花",

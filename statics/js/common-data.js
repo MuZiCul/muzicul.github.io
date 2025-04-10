b95a28383aee79dda8091fb1d6cff97c2983b420
obama <obama110@163.com>
2025-04-10 10:26:30 +0800

    更新数据

--- statics/js/common-data.js
+++ statics/js/common-data.js
@@ -40,7 +40,7 @@ const commonData = {
     // 网站访问密码配置
     passwordConfig: {
         enabled: true,                     // 是否启用密码功能
-        encodedPassword: "OTk2Ng==",  // base64编码的密码，解码后为"starfarm"
+        encodedPassword: "ODg0OA==",  // base64编码的密码，解码后为"starfarm"
         expireDays: 1,                     // Cookie过期天数
         title: "星宝农场必备手册 - 访问验证",  // 密码页面标题
         message: "请输入访问密码继续浏览",      // 密码页面提示信息

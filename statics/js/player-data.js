d9ece8ab8d0af09a94a0e8e11c65feaafffced58
obama <obama110@163.com>
2025-05-12 20:54:45 +0800

    更新温泉

--- statics/js/player-data.js
+++ statics/js/player-data.js
@@ -5,21 +5,21 @@ const playerData = {
         {
             title: "加工动物产品获得农场经验提升+24%",
             players: [
-                {id: "000", uid: "000", note: "", at: ""},
-                {id: "000", uid: "000", note: "", at: ""},
-                {id: "000", uid: "000", note: "", at: ""},
-                {id: "000", uid: "000", note: "", at: ""},
-                {id: "000", uid: "000", note: "", at: ""},
-                {id: "000", uid: "000", note: "", at: ""},
+                {id: "夏日菠萝啤", uid: "000", note: "", at: "沾砚"},
+                {id: "黒玫", uid: "000", note: "", at: ""},
+                {id: "童趣夜末雪", uid: "000", note: "", at: "半颜吖"},
+                {id: "Janelle芭啵", uid: "000", note: "", at: "鱼鱼崽崽z"},
+                {id: "雾缕烟", uid: "000", note: "", at: "姈钰"},
+                {id: "祈昭墨", uid: "000", note: "", at: ""},
 
             ]
         }, {
             title: "加工器容量提升+40%",
             players: [
-                {id: "000", uid: "000", note: "", at: ""},
-                {id: "000", uid: "000", note: "", at: ""},
-                {id: "000", uid: "000", note: "", at: ""},
-                {id: "000", uid: "000", note: "", at: ""},
+                {id: "沾砚", uid: "000", note: "", at: ""},
+                {id: "泛清波摘遍", uid: "000", note: "", at: ""},
+                {id: "像素再见源泉", uid: "000", note: "", at: ""},
+                {id: "我要揪你耳朵咯", uid: "000", note: "", at: "可可夹心芝士"},
                 {id: "000", uid: "000", note: "", at: ""},
                 {id: "000", uid: "000", note: "", at: ""},
             ]
@@ -27,10 +27,10 @@ const playerData = {
         {
             title: "钓鱼&鱼卡熟练度+60%",
             players: [
-                {id: "000", uid: "000", note: "", at: ""},
-                {id: "000", uid: "000", note: "", at: ""},
-                {id: "000", uid: "000", note: "", at: ""},
-                {id: "000", uid: "000", note: "", at: ""},
+                {id: "鲨鲨鲨鱼宝宝", uid: "000", note: "", at: ""},
+                {id: "媃嬵", uid: "000", note: "", at: ""},
+                {id: "Yao_冬", uid: "000", note: "", at: ""},
+                {id: "归眠恋玫瑰", uid: "000", note: "", at: ""},
                 {id: "000", uid: "000", note: "", at: ""},
                 {id: "000", uid: "000", note: "", at: ""},
             ]
@@ -38,17 +38,17 @@ const playerData = {
         {
             title: "农场丰收概率 【大】",
             players: [
-                {id: "000", uid: "000", note: "", at: ""},
-                {id: "000", uid: "000", note: "", at: ""},
-                {id: "000", uid: "000", note: "", at: ""},
-                {id: "000", uid: "000", note: "", at: ""},
-                {id: "000", uid: "000", note: "", at: ""},
-                {id: "000", uid: "000", note: "", at: ""},
+                {id: "啊啊啊啊啊pan", uid: "000", note: "", at: ""},
+                {id: "青烟睡不醒", uid: "000", note: "", at: "超级酷的知沂"},
+                {id: "君禧绾之h", uid: "000", note: "", at: ""},
+                {id: "深情in棒棒糖", uid: "000", note: "", at: "半颜吖"},
+                {id: "偷喝汽水ft", uid: "000", note: "", at: ""},
+                {id: "点听星火", uid: "000", note: "", at: ""},
             ]
         },{
             title: "每日拿取次数+15",
             players: [
-                {id: "000", uid: "000", note: "", at: ""},
+                {id: "小雪LLL", uid: "000", note: "", at: ""},
                 {id: "000", uid: "000", note: "", at: ""},
                 {id: "000", uid: "000", note: "", at: ""},
                 {id: "000", uid: "000", note: "", at: ""},
@@ -63,8 +63,8 @@ const playerData = {
         {
             title: "加工作物产品获得农场经验提升+30%",
             players: [
-                {id: "000", uid: "000", note: "", at: ""},
-                {id: "000", uid: "000", note: "", at: ""},
+                {id: "奶优宝宝", uid: "000", note: "", at: ""},
+                {id: "酱酱不吃姜yu", uid: "000", note: "", at: ""},
                 {id: "000", uid: "000", note: "", at: ""},
                 {id: "000", uid: "000", note: "", at: ""},
                 {id: "000", uid: "000", note: "", at: ""},
@@ -74,7 +74,7 @@ const playerData = {
         {
             title: "收获作物时农场经验获取提升+8%",
             players: [
-                {id: "000", uid: "000", note: "", at: ""},
+                {id: "听点星火", uid: "000", note: "", at: "点听星火"},
                 {id: "000", uid: "000", note: "", at: ""},
                 {id: "000", uid: "000", note: "", at: ""},
                 {id: "000", uid: "000", note: "", at: ""},
@@ -85,9 +85,9 @@ const playerData = {
         {
             title: "收获动物时农场经验获取提升+6%",
             players: [
-                {id: "000", uid: "000", note: "", at: ""},
-                {id: "000", uid: "000", note: "", at: ""},
-                {id: "000", uid: "000", note: "", at: ""},
+                {id: "不知名媛酱", uid: "000", note: "", at: ""},
+                {id: "柠檬爱汤圆", uid: "000", note: "", at: ""},
+                {id: "美女不讲道理", uid: "000", note: "", at: ""},
                 {id: "000", uid: "000", note: "", at: ""},
                 {id: "000", uid: "000", note: "", at: ""},
                 {id: "000", uid: "000", note: "", at: ""},
@@ -96,8 +96,8 @@ const playerData = {
         {
             title: "鱼塘全水层钓鱼经验+15%",
             players: [
-                {id: "000", uid: "000", note: "", at: ""},
-                {id: "000", uid: "000", note: "", at: ""},
+                {id: "深海", uid: "000", note: "", at: ""},
+                {id: "思相意", uid: "000", note: "", at: "醉心妍"},
                 {id: "000", uid: "000", note: "", at: ""},
                 {id: "000", uid: "000", note: "", at: ""},
                 {id: "000", uid: "000", note: "", at: ""},
@@ -111,7 +111,17 @@ const playerData = {
         {
             title: "作物熟练度获取提升+40%",
             players: [
-                {id: "000", uid: "000", note: "", at: ""},
+                {id: "喵喵喵哥哥怕怕", uid: "000", note: "", at: ""},
+                {id: "萌小只", uid: "000", note: "", at: "雪萌萌"},
+                {id: "i乌龙茶", uid: "000", note: "", at: "雪萌萌"},
+                {id: "小狗羔羔", uid: "000", note: "", at: ""},
+                {id: "me", uid: "000", note: "", at: ""},
+                {id: "嘤嘤嘤猪酱", uid: "000", note: "", at: ""},
+                {id: "纱猫讥笑", uid: "000", note: "", at: "6PJd"},
+                {id: "清野睡不醒", uid: "000", note: "", at: "超级酷的知沂"},
+                {id: "小脑菜菜", uid: "000", note: "", at: ""},
+                {id: "格子外的星空", uid: "000", note: "", at: ""},
+                {id: "吉士堡包", uid: "000", note: "", at: ""},
                 {id: "000", uid: "000", note: "", at: ""},
                 {id: "000", uid: "000", note: "", at: ""},
                 {id: "000", uid: "000", note: "", at: ""},
@@ -122,8 +132,8 @@ const playerData = {
         {
             title: "动物熟练度获取提升+40%",
             players: [
-                {id: "000", uid: "000", note: "", at: ""},
-                {id: "000", uid: "000", note: "", at: ""},
+                {id: "欧皇鸣鸣", uid: "000", note: "", at: ""},
+                {id: "无糖瑶瑶", uid: "000", note: "", at: "Yao_冬"},
                 {id: "000", uid: "000", note: "", at: ""},
                 {id: "000", uid: "000", note: "", at: ""},
                 {id: "000", uid: "000", note: "", at: ""},
@@ -138,9 +148,9 @@ const playerData = {
         {
             title: "水族箱效率提升+45%",
             players: [
-                {id: "000", uid: "000", note: "", at: ""},
-                {id: "000", uid: "000", note: "", at: ""},
-                {id: "000", uid: "000", note: "", at: ""},
+                {id: "是谁在被爱", uid: "000", note: "", at: ""},
+                {id: "打的你变猪头", uid: "000", note: "", at: ""},
+                {id: "星梦小小花花", uid: "000", note: "", at: ""},
                 {id: "000", uid: "000", note: "", at: ""},
                 {id: "000", uid: "000", note: "", at: ""},
                 {id: "000", uid: "000", note: "", at: ""},
@@ -149,10 +159,10 @@ const playerData = {
         {
             title: "鱼饵价格降低+15%",
             players: [
-                {id: "000", uid: "000", note: "", at: ""},
-                {id: "000", uid: "000", note: "", at: ""},
-                {id: "000", uid: "000", note: "", at: ""},
-                {id: "000", uid: "000", note: "", at: ""},
+                {id: "萌两只", uid: "000", note: "", at: "雪萌萌"},
+                {id: "超级酷的知沂", uid: "000", note: "", at: ""},
+                {id: "睡不着的咪", uid: "000", note: "", at: ""},
+                {id: "柠萌不萌oO", uid: "000", note: "", at: ""},
                 {id: "000", uid: "000", note: "", at: ""},
                 {id: "000", uid: "000", note: "", at: ""},
             ]
@@ -160,10 +170,10 @@ const playerData = {
         {
             title: "动物丰收概率提升 【大】",
             players: [
-                {id: "000", uid: "000", note: "", at: ""},
-                {id: "000", uid: "000", note: "", at: ""},
-                {id: "000", uid: "000", note: "", at: ""},
-                {id: "000", uid: "000", note: "", at: ""},
+                {id: "奶楹", uid: "000", note: "", at: ""},
+                {id: "是小夭哇", uid: "000", note: "", at: ""},
+                {id: "直觉委托喵咪", uid: "000", note: "", at: ""},
+                {id: "花花仔1", uid: "000", note: "", at: ""},
                 {id: "000", uid: "000", note: "", at: ""},
                 {id: "000", uid: "000", note: "", at: ""},
             ]

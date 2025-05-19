5f6f3c1600c2b5082329e539a1d69e64fe91d0e6
obama <obama110@163.com>
2025-05-19 09:49:54 +0800

    更新温泉，增加空白模版

---
+++ statics/js/merchant-data-null.js
@@ -0,0 +1,112 @@
+// 商人数据文件
+const merchantData = {
+    // 商人数据
+    merchants: [
+        //       {
+        //     type: "请移步游戏内【派对】板块查找商人",
+        //     players: [
+        //         { id: "请移步游戏内【派对】板块查找商人", uid: "000", sell:"2.0", at:"", note: "", leaveTime: "12.31号23.59" },
+        //         { id: "Gigima", uid: "517508000", sell:"1.9", at:"不要不开心噢7", note: "", leaveTime: "4.8号14.49" },
+        //         { id: "好奇了吗", uid: "522348200", sell:"1.9", at:"", note: "", leaveTime: "4.8号18.08" },
+        //         { id: "洗白", uid: "1708851718", sell:"1.9", at:"", note: "", leaveTime: "4.6号19.29" },
+        //     ]
+        // },
+        {
+            type: "连翘",
+            players: [
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+            ]
+        },{
+            type: "粉目菊",
+            players: [
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+            ]
+        },{
+            type: "莲花掌",
+            players: [
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+            ]
+        },{
+            type: "仙客来",
+            players: [
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+            ]
+        },{
+            type: "黄百合",
+            players: [
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+            ]
+        },{
+            type: "丁香花",
+            players: [
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+            ]
+        },{
+            type: "康乃馨",
+            players: [
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+            ]
+        },{
+            type: "雪滴花",
+            players: [
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+            ]
+        },{
+            type: "夏腊梅",
+            players: [
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+            ]
+        },{
+            type: "虞美人",
+            players: [
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+            ]
+        },{
+            type: "黄色郁金香",
+            players: [
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+            ]
+        },{
+            type: "紫色风信子",
+            players: [
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+                { id: "000", uid: "000", sell:"2.0", at:"000", note: "", leaveTime: "4.6号15.57" },
+            ]
+        },
+    ]
+}; 
